03a36265d81c15acedaae9b18abac2f24a4f99fa
Aravind Kannan Rathinasabapathi <55327432+Aravind-Kannan@users.noreply.github.com>
2022-01-18 12:51:50 +0530

    Added upto Lec1:Part1

---
+++ Week-01/square.cpp
@@ -0,0 +1,18 @@
+#include <simplecpp>
+
+main_program
+{
+    turtleSim();
+    forward(100);
+    right(90);
+    wait(1);
+    forward(100);
+    right(90);
+    wait(1);
+    forward(100);
+    right(90);
+    wait(1);
+    forward(100);
+
+    wait(5);
+}
---
+++ Week-01/triangle.cpp
@@ -0,0 +1,15 @@
+#include <simplecpp>
+
+main_program
+{
+    turtleSim();
+    forward(100);
+    right(120);
+    wait(1);
+    forward(100);
+    right(120);
+    wait(1);
+    forward(100);
+
+    wait(5);
+}
